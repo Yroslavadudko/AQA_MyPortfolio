0e32566f315b95cf0b33a01f565ee0bc1cf9b291
Yroslavadudko <dudkomykola@icloud.com>
2023-10-28 19:41:23 +0300

    Form email

--- src/test/java/MyPortfolio/MyTest.java
+++ src/test/java/MyPortfolio/MyTest.java
@@ -30,8 +30,6 @@ public class MyTest {
   public void testMykolaDudkoPage(){
     driver.get("https://mykoladudko.netlify.app");
 
-    WebElement headerElement = driver.findElement(By.tagName("h1"));
-
     //Title check
     WebElement homeLink = driver.findElement(By.xpath("//nav[@class='navbar']//a[text()='home']"));
     homeLink.click();
@@ -39,6 +37,7 @@ public class MyTest {
     assertEquals(driver.getTitle(), expectedPageTitle, "The page title does not match the expected text");
 
     //Checking the navigation menu/Home
+    WebElement headerElement = driver.findElement(By.tagName("h1"));
     String expectedHeaderText = "hello, i'm Mykola";
     String actualHeaderText = headerElement.getText();
     assertEquals(actualHeaderText, expectedHeaderText, "The page title does not match the expected text");
@@ -50,41 +49,62 @@ public class MyTest {
     wait.until(ExpectedConditions
             .visibilityOfElementLocated(By.xpath("//nav[@class='navbar']//a[text()='projects']")));
     WebElement projectsHeader = driver
-            .findElement(By.xpath("//section[@class='project-section active']//h1[@class='project-heading']"));
+            .findElement(By.xpath(
+                    "//section[@class='project-section active']//h1[@class='project-heading']"));
     String expectedProjectsHeaderText = "some of my projects";
     String actualProjectsHeaderText = projectsHeader.getText();
-    assertEquals(actualProjectsHeaderText,expectedProjectsHeaderText, "The page title does not match the expected text");
+    assertEquals(actualProjectsHeaderText,expectedProjectsHeaderText,
+            "The page title does not match the expected text");
 
     //Checking the navigation menu/About
     WebElement aboutLink = driver.findElement(By.xpath("//nav[@class='navbar']//a[text()='about']"));
     aboutLink.click();
-      //Find the "my about" element using JavaScript
+        //Find the "my about" element using JavaScript
     JavascriptExecutor js = (JavascriptExecutor) driver;
-    String aboutHeader = (String) js.executeScript("return document.querySelector('section.about-section.active h1[hidden]').textContent");
+    String aboutHeader = (String) js.executeScript
+            ("return document.querySelector('section.about-section.active h1[hidden]').textContent");
     String expectedAboutHeaderText = "my about";
     assertEquals(aboutHeader, expectedAboutHeaderText, "The page title does not match the expected text");
         //Click on the body of the page to lose focus on "Projects"
     js.executeScript("document.body.click()");
         //Find the "skills" section
-    WebElement skillsSection = driver.findElement(By.xpath("//div[@class='skill-section']//h1[@class='heading']"));
+    WebElement skillsSection = driver.
+            findElement(By.xpath("//div[@class='skill-section']//h1[@class='heading']"));
     String expectedSkillsHeaderText = "skills";
     String actualSkillsHeaderText = skillsSection.getText();
-    assertEquals(actualSkillsHeaderText, expectedSkillsHeaderText, "The page title does not match the expected text");
+    assertEquals(actualSkillsHeaderText, expectedSkillsHeaderText,
+            "The page title does not match the expected text");
         //Find the "education and experience" section
-    WebElement educationAndExperienceSection = driver.findElement(By.xpath("//div[@class='timeline']//h1[@class='heading']"));
+    WebElement educationAndExperienceSection = driver.
+            findElement(By.xpath("//div[@class='timeline']//h1[@class='heading']"));
     String expectedEducationAndExperienceHeaderText = "education and experience";
     String actualEducationAndExperienceHeaderText = educationAndExperienceSection.getText();
-    assertEquals(actualEducationAndExperienceHeaderText, expectedEducationAndExperienceHeaderText, "The page title does not match the expected text");
+    assertEquals(actualEducationAndExperienceHeaderText, expectedEducationAndExperienceHeaderText,
+            "The page title does not match the expected text");
 
     //Checking the navigation menu/Contact
     WebElement contactLink = driver.findElement(By.xpath("//nav[@class='navbar']//a[text()='contact']"));
     contactLink.click();
         //Find the "contact" element using JavaScript
-    String contactHeader = (String) js.executeScript("return document.querySelector('section.contact-section h1').textContent");
+    String contactHeader = (String) js.executeScript
+            ("return document.querySelector('section.contact-section h1').textContent");
     String expectedContactHeaderText = "Contact Information";
     assertEquals(contactHeader, expectedContactHeaderText, "The page title does not match the expected text");
         //Click on the body of the page to lose focus on "my about"
     js.executeScript("document.body.click()");
+    //Find the contact form
+    WebElement contactForm = driver.findElement(By.cssSelector("form.contact-form"));
+        //Find the name, email, and message elements within the form
+    WebElement nameInput = driver.findElement(By.id("name"));
+    WebElement emailInput = driver.findElement(By.id("email"));
+    WebElement messageInput = driver.findElement(By.id("msg"));
+        //Find in the form fields
+    nameInput.sendKeys("Myk");
+    emailInput.sendKeys("myk@myk.mk");
+    messageInput.sendKeys("Hello, this is a AQA test");
+        //Find and click the submit button
+    WebElement submitButton = contactForm.findElement(By.cssSelector("button.form-submit-btn"));
+    submitButton.click();
 
 
 
