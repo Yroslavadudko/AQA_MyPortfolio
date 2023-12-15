cdcd7c5b13b0336924dbb266f40ea15f1fec0042
Yroslavadudko <dudkomykola@icloud.com>
2023-12-15 11:35:06 +0200

    Home Page test

--- src/test/java/MyPortfolio/TestHomePage.java
+++ src/test/java/MyPortfolio/TestHomePage.java
@@ -11,21 +11,22 @@ import static org.testng.Assert.assertEquals;
 public class TestHomePage extends BaseTest {
     @Test(groups = "allTests")
     public void testHomePage() {
-
-        //Title check
+        // Title check
         WebElement homeLink = driver.findElement(By.cssSelector("body:nth-child(2) nav.navbar:nth-child(1) ul.link-group li.link.active:nth-child(1) > a:nth-child(1)"));
         homeLink.click();
+
         String expectedPageTitle = "Mykola Dudko - Quality Assurance Engineer";
         assertEquals(driver.getTitle(), expectedPageTitle, "The page title does not match the expected text");
 
-
-        //Checking the navigation menu/Home
+        // Checking the navigation menu/Home
         WebElement bodyElement = driver.findElement(By.xpath("//body/section[1]/h1[1]"));
-        String expectedBodyText = "Hello, I'M\n Mykola";
-        String actualBodyText = bodyElement.getText();
-        assertEquals(actualBodyText, expectedBodyText, "The page title does not match the expected text");
+        String expectedBodyText = "hello, i'm\nmykola";
+        String actualBodyText = bodyElement.getText().toLowerCase().trim();
+
+        assertEquals(actualBodyText, expectedBodyText, "The body text does not match the expected text");
 
         Assert.assertTrue(homeLink.isDisplayed());
         assertEquals(homeLink.getText(), "Home");
     }
+
 }
